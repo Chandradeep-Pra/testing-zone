f1b8b37a55b66afe256f6a2a20ae7614aa96b9fe
chandradeep-pra <chandradeepprasad1998@gmail.com>
2026-03-06 03:44:11 +0530

    Enforce to use cred from env in vercel

--- app/api/viva/tts/route.ts
+++ app/api/viva/tts/route.ts
@@ -1,29 +1,68 @@
 //@ts-nocheck
 
-import { TextToSpeechClient } from "@google-cloud/text-to-speech";
+// import { TextToSpeechClient } from "@google-cloud/text-to-speech";
 
-const client = new TextToSpeechClient();
+// const client = new TextToSpeechClient();
 
-export async function POST(req: Request) {
+// export async function POST(req: Request) {
+//   const { text } = await req.json();
+
+//   const [response] = await client.synthesizeSpeech({
+//     input: { text },
+
+//     voice: {
+//       languageCode: "en-US",
+//       name: "en-US-Chirp3-HD-Charon",
+//     },
+
+//     audioConfig: {
+//   audioEncoding: "LINEAR16",
+//   sampleRateHertz: 24000
+// }
+//   });
+
+//   return new Response(response.audioContent as Uint8Array, {
+//     headers: {
+//       "Content-Type": "audio/wav",
+//     },
+//   });
+// }
+
+import textToSpeech from "@google-cloud/text-to-speech";
+
+const raw = process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON;
+
+if (!raw) {
+  throw new Error("Missing GOOGLE_APPLICATION_CREDENTIALS_JSON");
+}
+
+const creds = JSON.parse(raw);
+
+const client = new textToSpeech.TextToSpeechClient({
+  credentials: {
+    client_email: creds.client_email,
+    private_key: creds.private_key.replace(/\\n/g, "\n"),
+  },
+  projectId: creds.project_id,
+});
+
+export async function POST(req) {
   const { text } = await req.json();
 
   const [response] = await client.synthesizeSpeech({
     input: { text },
-
     voice: {
       languageCode: "en-US",
       name: "en-US-Chirp3-HD-Charon",
     },
-
     audioConfig: {
-  audioEncoding: "LINEAR16",
-  sampleRateHertz: 24000
-}
+      audioEncoding: "MP3",
+    },
   });
 
-  return new Response(response.audioContent as Uint8Array, {
+  return new Response(response.audioContent, {
     headers: {
-      "Content-Type": "audio/wav",
+      "Content-Type": "audio/mpeg",
     },
   });
 }
